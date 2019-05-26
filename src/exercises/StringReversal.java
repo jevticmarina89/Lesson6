82ff53df552ff24b5f5397416a41b0ff4090f740
jevticmarina89 <marina.jevtic89@gmail.com>
2019-05-26 12:38:47 +0200

    lasreveRgnirtS made

--- src/exercises/StringReversal.java
+++ src/exercises/StringReversal.java
@@ -1,5 +1,8 @@
 package exercises;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import junit.framework.TestCase;
 
 public class StringReversal extends TestCase {
@@ -11,8 +14,22 @@ public class StringReversal extends TestCase {
 	 *       QcXgW9w4wQd=v?hctaw/moc.ebutuoy
 	 */
 
-//	public void test() {
-//		assertEquals("cellardoor", reverseMe("roodrallec"));
-//	}
+
+	public String reverseMe(String str) {
+		
+		char [] letterArray = str.toCharArray();
+		String forward = "";
+		for(int i=letterArray.length-1; i>=0; i--) {
+			forward += letterArray[i]; 
+	}
+	return forward;	
+	}
+	
+	public void test() {
+		assertEquals("cellardoor", reverseMe("roodrallec"));
+		
+		System.out.println(reverseMe("QcXgW9w4wQd=v?hctaw/moc.ebutuoy"));
+	}
+	
 
 }
