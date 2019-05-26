023efa6bed36793463d85d3bade5029e298b1229
jevticmarina89 <marina.jevtic89@gmail.com>
2019-05-26 12:54:31 +0200

    hand-crafted ornaments

--- src/exercises/Ornament.java
+++ src/exercises/Ornament.java
@@ -3,13 +3,22 @@ package exercises;
 /* 
  * Create a class called Decorator that contains an ArrayList called boxOfDecorations.
  *   1. In the Decorator class, hang all the ornaments in the box.
- *   2. Add a color to the Ornament class so that your program prints “Hanging a red ornament”, etc.
+ *   2. Add a color to the Ornament class so that your program prints â€œHanging a red ornamentâ€�, etc.
  */
 
 public class Ornament {
 	
+	String color;
+	String type;
+	
+	public Ornament(String color, String type) {
+		this.color = color;
+		this.type = type;
+	}
+
 	public void hang(){
-		System.out.println("hanging an ornament");
+		System.out.println("hanging a " + color + " ornament " + type);
 	}
 
 }
+
